e7a70a474a83b80ca49545739f8223e9952d29e8
Tatvam <td14@iitbbs.ac.in>
2019-03-02 20:20:33 +0530

    Shader

--- main.cpp
+++ main.cpp
@@ -5,8 +5,41 @@
 #include<glm/glm.hpp>
 #include<GLFW/glfw3.h>
 #include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
 
 using namespace std;
+
+struct ShaderProgramSource{
+    string VertexSource;
+    string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const string& filepath){
+
+    ifstream stream(filepath);
+
+    enum class ShaderType{
+        NONE = -1, VERTEX = 0, FRAGMENT = 1 
+    };
+
+    string line;
+    stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+    while(getline(stream,line)){
+        if(line.find("#shader")!=string::npos){
+            if(line.find("vertex")!=string::npos){
+                type = ShaderType::VERTEX;
+            }else if(line.find("fragment")!=string::npos){
+                type = ShaderType::FRAGMENT;
+            }
+        }else{
+            ss[(int)type] <<line << '\n';
+        }
+    }
+    return {ss[0].str(), ss[1].str()};
+}
 
 static unsigned int CompileShader(unsigned int type, const string& source){
 
@@ -107,26 +140,10 @@ int main(){
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
 
-    string vertexShader = 
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) in vec4 position;"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = position;\n"
-        "}\n";
-
-     string fragmentShader = 
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) out vec4 color;"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-        "}\n";
-    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    ShaderProgramSource source = ParseShader("res/shaders/Shader.shader");
+    cout<<source.VertexSource<<endl;
+    cout<<source.FragmentSource<<endl;
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
 
     //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -144,4 +161,9 @@ int main(){
         glfwPollEvents();
     }
     while(glfwGetKey(window,GLFW_KEY_ESCAPE)!=GLFW_PRESS && glfwWindowShouldClose(window)==0);
+
+    glDeleteProgram(shader);
+
+    glfwTerminate();
+    return 0;
 }
